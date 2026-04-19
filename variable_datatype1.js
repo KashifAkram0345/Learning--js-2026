ff2b1e6af91756ef97052b8ff7a7232caf57fa48
KashifAkram0345 <m.kashif6833@gmail.com>
2026-04-19 13:14:02 +0500

    variable && datatype

---
+++ variable_datatype1.js
@@ -0,0 +1,39 @@
+// var age=20;
+// console.log(age); //gobal scope
+
+// var age = 22;
+// if (true) {
+//     console.log(age)
+// }
+
+// let age=23;
+// console.log(age)
+
+// const a=2;
+// console.log(a)
+
+
+
+
+// {
+//     let age=34;
+//     console.log(age);
+// }
+
+
+// let num=30;
+// console.log(num);
+
+// let str="kashif"
+// console.log(str);
+
+// let bole=true;
+// console.log(bole);
+
+// let age;
+// console.log(age)
+let khali=null;
+console.log(khali)
+
+let sym='@'
+console.log(sym)
