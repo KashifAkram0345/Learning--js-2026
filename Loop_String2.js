9ec91b4cf04717d1dae59bcaa3f4330c821fed7c
KashifAkram0345 <m.kashif6833@gmail.com>
2026-04-19 16:53:26 +0500

    Loop_String

---
+++ Loop_String2.js
@@ -0,0 +1,82 @@
+// for loop
+
+// for(let i=1;i<=10;i++){
+//     console.log("Kashif")
+// }
+// for(let i=1;i<=10;i++){
+//     console.log(i);
+// }
+// for(let i=5;i>0;i--){
+//     console.log(i)
+// }
+
+// for(let i=1;i<=6;i++){
+//     if(i==4){
+//         break;
+
+//     }
+//     else{
+//         console.log(i)
+//     }
+// }
+
+
+// let i=1;
+// while(i<=5){
+//     console.log("kashif");
+//     i++
+// }
+
+// let i=1;
+// while(i<=10){
+    // console.log(i)
+    // i++;
+// }
+//  let i=1;
+//  while(i<=6){
+//     if(i==4){
+//         break;
+//     }
+//     else{
+//         console.log(i);
+//         i++;
+//     }
+// //  }
+
+// let str="kashif"
+// console.log(str);
+// let str='Kashif Akram'
+// console.log(str)
+// let FirstName=new String("kashif")
+// console.log(FirstName)
+// let FirstName="kashif"
+// let LastName="Akram";
+// console.log(FirstName+LastName)
+// let FirstName="kashif"
+// console.log(FirstName.length)
+// let SecondName="kashif"
+// console.log(SecondName.toUpperCase())
+// let SecondName="kashif"
+// console.log(SecondName.toLowerCase())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
