12661b498d27a9681a37237f59da9d8424fff159
KashifAkram0345 <m.kashif6833@gmail.com>
2026-04-21 21:11:44 +0500

    09-Question

---
+++ Assignment_Loop_String/09_reverse_string.js
@@ -0,0 +1,7 @@
+// Q.9 Replace all spaces with - without using replaceAll().
+
+
+let str = 'Kashif';
+for (let i = str.length - 1; i >= 0; i--) {
+    console.log(str[i]);
+}                                                             
