991eb240188fa29792bc8a500ce22159c86a4588
KashifAkram0345 <m.kashif6833@gmail.com>
2026-05-01 16:57:15 +0500

    Question 5

---
+++ Assignment_loops_strings/05_specific_chacter.js
@@ -0,0 +1,32 @@
+// 5. Count how many times a specific character appears in a string.
+//     * Input: "banana", "a" → 3
+
+// let str = "banana";
+// let Count=0;
+// for(let i=0;i<=str.length;i++){
+//     if(str[i]==='a'){
+//         Count++;
+//     }
+// }
+// console.log(Count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+let str='banana';
+let count=0;
+for(let i=0;i<str.length;i++){
+    if(str==='a'){
+        count++;
+    }
+}
+console.log(count)
